31be5d2f1ffcef860d14fde16d56bec2750eebc9
Nodecir Pacheco dos Santos <nodecir@yahoo.com.br>
2023-01-08 15:38:55 -0300

    Adicionando os testes de Menu e Perfil

---
+++ cypress/e2e/Corona/Perfil.cy.js
@@ -0,0 +1,64 @@
+///<reference types="cypress"/>
+
+describe('PERFIL BRASIL', () => {
+    before(() => {
+        cy.viewport(1920, 1080)
+        cy.visit('https://coronacom.test-abinbev.acsitefactory.com/pt-br/sunsets23')
+        cy.wait(500)
+    })
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        if (err.message.includes('li')) {
+            return false
+        }
+    })
+    describe('login/log out', () => {
+        it('A ferramenta deve realizar o login e logo depois realizar log out', () => {
+            cy.get('#age-gate--yes').click() // Clicar no "SIM" no age gate
+
+            cy.get('.coh-ce-cpt_header_hamburger_button_fwc-e6b7b2b3 > .login-header-link > .ssa-component > .coh-ce-cpt_button-a384e411 > .paragraph-container-horizontal-none > .coh-container > .coh-link').click({ force: true })
+
+            cy.get('#edit-email').type('rodrigo.ribeiro+brasil1@justdigital.com.br')
+            cy.get('#edit-password').type('Teste123')
+            cy.get('#edit-submit').click({ force: true })
+
+            cy.request({
+                method: 'POST',
+                url: 'https://coronacom.test-abinbev.acsitefactory.com/pt-br/sunsets23/user/sso/entrar', // baseUrl is prepend to URL
+                // failOnStatusCode: false,
+                form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+                body: {
+                    email: 'rodrigo.ribeiro+brasil1@justdigital.com.br',
+                    password: 'Teste123',
+                },
+            }).then(
+                (response) => {
+                    // response.body is automatically serialized into JSON
+                    expect(response.status).to.eq(200) // true
+                    expect(response).to.have.property('headers')
+                    // expect(response.body).to.be.equal({password:'123Mudar#'})
+                }
+            )
+
+            cy.request({
+                method: 'GET',
+                url: 'https://coronacom.test-abinbev.acsitefactory.com/pt-br/sunsets23/minha-conta', // baseUrl is prepend to URL
+                failOnStatusCode: false,
+                form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+                body: {
+                    email: 'rodrigo.ribeiro+brasil1@justdigital.com.br',
+                    password: 'Teste123',
+                },
+            }).then(
+                (response) => {
+                    // response.body is automatically serialized into JSON
+                    expect(response.status).to.eq(200) // true
+                    expect(response).to.have.property('headers')
+                    // expect(response.body).to.be.equal({password:'123Mudar#'})
+                }
+            )
+            cy.get('.coh-ce-cpt_header_hamburger_button_fwc-e6b7b2b3 > .profile-header-link > .ssa-component > .coh-ce-cpt_button-a384e411 > .paragraph-container-horizontal-none > .coh-container > .coh-link').click({ force: true })
+            cy.get('.has-logout > .coh-link').click({ force: true })
+        })
+    })
+})
