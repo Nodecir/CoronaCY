ac417b6754e6e2a0625ab5e35bef18ec22ba7c75
Nodecir Pacheco dos Santos <nodecir@yahoo.com.br>
2023-01-13 01:20:28 -0300

    Adding Report

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 // import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
+  projectId: "9h3cxt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
